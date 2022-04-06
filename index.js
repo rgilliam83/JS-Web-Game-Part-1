adfe818db07d96688c2e47ba85235f296c243dcd
Rashad Gilliam <akklaim83@gmail.com>
2022-04-06 00:54:00 -0700

    changes

--- index.js
+++ index.js
@@ -9,15 +9,5 @@ function newImage (path, left, bottom){
 }
 
 
-
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
-
-
-
-
+newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
