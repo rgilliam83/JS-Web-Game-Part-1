8c55dc4b7ce4aabfa2c921fc95e0d3918abbec83
Rashad Gilliam <akklaim83@gmail.com>
2022-04-07 23:52:01 -0700

    changes

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+
 function newImage (path, left, bottom){
     let image = document.createElement('img')
     image.src = path
     image.style.position = 'fixed'
-    image.style.left = '450px'
-    image.style.bottom = '200px'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
     document.body.append(image)  
+    return image
     
+}
+
+image.addEventListener('dblclick', function (){
+    image.remove()
+})
+
+function newItem (path, left, bottom){
+    let item = document.createElement('img')
+    item.src = path
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })
 }
 
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
